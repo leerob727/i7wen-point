7725566c91ebbfcf315e4daa5098fa7f77531890
myselfsourav <sourav.das@pkweb.in>
2015-03-17 13:34:54 +0530

    Update single.php

--- wp-content/themes/point/single.php
+++ wp-content/themes/point/single.php
@@ -67,8 +67,7 @@
 echo '<div style="margin-bottom:8px;overflow:hidden;"><div style="display:inline-block;padding-right:5px;" class="fb-like" data-href="https://www.facebook.com/pages/&#x7231;&#x5947;&#x95fb;-i7wencom/399615256870590" data-layout="button" data-action="like" data-show-faces="true" data-share="false"></div>
 
 <div class="fb-share-button" data-href="' . $_SERVER["REQUEST_URI"] . '" data-layout="button"></div></div>'; //Facebook
-} ?>
-
+} //RLEE ?>
 <?php rlee_after_pagination(); //RLEE ?>
 							<?php if ($mts_options['mts_postend_adcode'] != '') { ?>
 								<?php $endtime = $mts_options['mts_postend_adcode_time']; if (strcmp( date("Y-m-d", strtotime( "-$endtime day")), get_the_time("Y-m-d") ) >= 0) { ?>
